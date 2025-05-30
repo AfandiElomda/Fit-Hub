d2188b7182460caa1fafcff122589d574ee4a8e2
Afandi Elomda <afandielomda@gmail.com>
2025-05-30 07:05:29 +0300

    installation shadcnui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div>
+      <h1 className='text-2xl underline'>Hello,Welcome to Our Fitness Space</h1>
+      <Button>Explore</Button>
+    </div>
   )
 }
 
